7045604fa226a03ca133f6a7816d47fbcabc2489
Melonours <adrianrumi@hotmail.com>
2025-01-16 14:18:59 +0100

    js fini

--- public/js/modules/fonctions.js
+++ public/js/modules/fonctions.js
@@ -4,40 +4,60 @@ function hello(){
     console.log("salut depuis la fctn hello");
 }
 
-// ! Fonction qui va incrémenter et afficher le résultat tout les X temps
-
-function counter(e) {
-    let joe = e.innerText
-    // console.log(joe);
+// ! Fonction qui va incrémenter et afficher le résultat
+// * Fonctionne mais ne se termine pas au même moment
+// function counter(e) {
+//     let joe = e.innerText
+//     // console.log(joe);
     
-    e.innerText = 0
-    // console.log(e.innerText);
+//     e.innerText = 0
+//     // console.log(e.innerText);
 
-    let interval = setInterval( ()=> {
-        e.innerText ++
+//     let interval = setInterval( ()=> {
+//         e.innerText ++
 
-        if(e.innerText == joe){
-            clearInterval(interval)
-        }
+//         if(e.innerText >= joe){
+//             clearInterval(interval)
+//         }
 
-    }, 0)
+//     }, 0)
 
-}
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     setTimeout( () => {
+//         counter(twitterNbr)
+//         counter(youtubeNbr)
+//         counter(facebookNbr)
+//     }, 2000)
+// })
+
+
+
+
+// ! Fonction qui va incrémenter et afficher le résultat
+function count(value,e){
+    let startingNbr = 0;
+    let animationNbr = value / 100;
+    
+    let interval = setInterval(()=>{
+      if(startingNbr >= value - animationNbr){
+        clearInterval(interval);
+      }
+      startingNbr+=animationNbr;
+      e.innerHTML = Math.round(startingNbr);
+    },30);
+  };
 
 
 // ! Fonction qui va lancer le tout après 2 sec
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout( () => {
-        counter(twitterNbr)
-        counter(youtubeNbr)
-        counter(facebookNbr)
+        count(12000,twitterNbr)
+        count(5000, youtubeNbr)
+        count(7500, facebookNbr)
     }, 2000)
 })
-
-// counter(twitterNbr)
-// console.log(twitterNbr.innerText);
-// console.log(youtubeNbr.innerText);
-// console.log(facebookNbr.innerText);
 
 
 export { hello }
