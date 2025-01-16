0eb7e5682f89aecc368f856cf05ddb43b310cefa
Melonours <adrianrumi@hotmail.com>
2025-01-16 13:48:57 +0100

    js presque fini

--- public/js/modules/fonctions.js
+++ public/js/modules/fonctions.js
@@ -4,10 +4,37 @@ function hello(){
     console.log("salut depuis la fctn hello");
 }
 
-// ! Fonction qui va incrémenter
+// ! Fonction qui va incrémenter et afficher le résultat tout les X temps
+
+function counter(e) {
+    let joe = e.innerText
+    // console.log(joe);
+    
+    e.innerText = 0
+    // console.log(e.innerText);
+
+    let interval = setInterval( ()=> {
+        e.innerText ++
+
+        if(e.innerText == joe){
+            clearInterval(interval)
+        }
+
+    }, 0)
+
+}
 
 
+// ! Fonction qui va lancer le tout après 2 sec
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout( () => {
+        counter(twitterNbr)
+        counter(youtubeNbr)
+        counter(facebookNbr)
+    }, 2000)
+})
 
+// counter(twitterNbr)
 // console.log(twitterNbr.innerText);
 // console.log(youtubeNbr.innerText);
 // console.log(facebookNbr.innerText);
